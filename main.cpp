f21bd82f3a6914464101545b3a1f0165daf4aac9
Skycake_ <ribarrow.171@gmail.com>
2026-07-10 00:58:48 -0500

    setup window stuff

--- main.cpp
+++ main.cpp
@@ -1,13 +1,36 @@
 #include <GLFW/glfw3.h>
+#include <cstdio>
 #include <iostream>
+
+void error_callback(int error, const char* description);
 
 int main(int argc, char *argv[]){
 
+    glfwSetErrorCallback(error_callback);
+
     if(!glfwInit()){
-	std::cout << "meow";
-    glfwTerminate();
+
+	    std::cout << "glfw Initialisation failed";
+        glfwTerminate();
+
     } else {
-	std::cout<<"meow2";
+
+	    std::cout<<"glfw init sucessful yoohoo!";
+
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        
+        GLFWwindow* window = glfwCreateWindow(640, 480, "Yo!", NULL, NULL);
+        if(!window)
+        {
+            std::cout << "window failed to open";
+        }
+    
     }
 
+}
+
+void error_callback(int error, const char* description){
+    fprintf(stderr, "Error: %s\n", description);
 }
