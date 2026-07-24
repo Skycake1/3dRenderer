ca3bc2c6eef13624626aec8660134af07b75f276
Skycake_ <ribarrow.171@gmail.com>
2026-07-23 21:44:19 -0500

    completed first four sections of the TODO list (shaders + triangle render)

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
 #include <iostream>
 
 void error_callback(int error, const char* description);
@@ -13,6 +14,29 @@ void processInput(GLFWwindow *window);
 const int width = 640;
 const int height = 480;
 
+
+
+unsigned int vertexShader;
+
+
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+
+const char *fragmentShaderSource = "#version 330 core \n"
+    "out vec4 FragColor; \n"
+    "void main()\n"
+    "{\n"
+        "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\0";
+
+
+
+
 int main(int argc, char *argv[]){
 
     glfwSetErrorCallback(error_callback);
@@ -25,9 +49,6 @@ int main(int argc, char *argv[]){
         glfwTerminate();
     } else {
 
-
-
-
 	    std::cout<<"glfw init sucessful yoohoo! \n";
 
         //window hint functionally act as little settings for the window
@@ -39,9 +60,9 @@ int main(int argc, char *argv[]){
         GLFWwindow* window = glfwCreateWindow(width, height, "Yo!", nullptr, nullptr);
         if(window != nullptr)
         {
-            std::cout << "window created successfully";
+            std::cout << "window created successfully \n";
         } else {
-            std::cout << "window failed to open";
+            std::cout << "window failed to open \n";
         }
 
         //makes glfw look at our current context
@@ -53,12 +74,102 @@ int main(int argc, char *argv[]){
             std::cout << "Failed to initialize GLAD" << std::endl;
             glfwTerminate();
             return -1;
+        } else {
+            std::cout << "GLAD intialized correctly" << std::endl;
         }
 
         //basically our canvas size, and we want it to be the same as the window size
         glViewport(0, 0, width, height);
         //which is why we setup our callback function right here
         glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+        //shader setup
+
+            //vertex Shader--------------------------------------------------------------------
+
+                vertexShader = glCreateShader(GL_VERTEX_SHADER);
+                glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+                glCompileShader(vertexShader);
+
+            int  success;
+            char infoLog[512];
+                glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+                if(!success)
+                {
+                    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+                    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+                } else {
+                    std::cout << "Vertex Shader Loaded Successfully" << '\n';
+                }
+
+            //fragment shader-------------------------------------------------------------------
+
+                unsigned int fragmentShader;
+                fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+                glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+                glCompileShader(fragmentShader);
+
+
+                glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+                if(!success)
+                {
+                    glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+                    std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+                } else {
+                    std::cout << "Fragment Shader Loaded Successfully" << '\n';
+                }
+
+
+        //shader program(basically linking these the fragment and vertex shader together)
+
+            unsigned int shaderProgram;
+            shaderProgram = glCreateProgram();
+
+            glAttachShader(shaderProgram, vertexShader);
+            glAttachShader(shaderProgram, fragmentShader);
+            glLinkProgram(shaderProgram);
+
+
+
+            glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+            if(!success) {
+                glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+                std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+            }else {
+                std::cout << "Shader Program Linked Successfully" << '\n';
+            }
+
+            //we delete the shaders once we are done so they don't take up any more space
+            glDeleteShader(vertexShader);
+            glDeleteShader(fragmentShader);
+
+        const float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+        };
+
+        //VBO is the literal data and VAO is how to process it
+
+        unsigned int VBO, VAO;
+
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+
+        glBindVertexArray(VAO);
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+        //basically a rough explanation of the vertexes that we are tossing in
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+
+
 
 
         //Render Loop
@@ -68,8 +179,19 @@ int main(int argc, char *argv[]){
             processInput(window);
 
             //Rendering Commands Here
-            glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+            glUseProgram(shaderProgram);
+
+
+            glClearColor( 0.16f, 0.01f, 0.11f, 0.5f);
             glClear(GL_COLOR_BUFFER_BIT);
+
+            glBindVertexArray(VAO);
+
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+
+
+
+
 
             //check and call events and swap buffers
             glfwSwapBuffers(window);
@@ -78,6 +200,7 @@ int main(int argc, char *argv[]){
 
 
 
+        std::cout<< "\n-----------------------------------------------------------\n";
         glfwTerminate();
         return 0;
     }
