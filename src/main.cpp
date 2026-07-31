637c875fdda3ca658da11b2f5cb81bead86b0049
Skycake_ <ribarrow.171@gmail.com>
2026-07-30 23:54:57 -0500

    added 3d stuff and some other thingos

--- src/main.cpp
+++ src/main.cpp
@@ -38,6 +38,19 @@ unsigned int fragmentShader;
 std::string currentPath = std::filesystem::current_path();
 
 
+glm::vec3 cubePositions[] = {
+    glm::vec3( 0.0f,  0.0f,  0.0f),
+    glm::vec3( 2.0f,  5.0f, -15.0f),
+    glm::vec3(-1.5f, -2.2f, -2.5f),
+    glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3( 2.4f, -0.4f, -3.5f),
+    glm::vec3(-1.7f,  3.0f, -7.5f),
+    glm::vec3( 1.3f, -2.0f, -2.5f),
+    glm::vec3( 1.5f,  2.0f, -2.5f),
+    glm::vec3( 1.5f,  0.2f, -1.5f),
+    glm::vec3(-1.3f,  1.0f, -1.5f)
+};
+
 
 
 int main(int argc, char *argv[]){
@@ -95,7 +108,8 @@ int main(int argc, char *argv[]){
         //which is why we setup our callback function right here
         glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-
+        //enable depth test
+        glEnable(GL_DEPTH_TEST);
         //shader setup
 
         Shader ourShader((currentPath + "shaders/vertex/vertexShader.vs").c_str(),(currentPath+ "shaders/fragment/fragmentShader.fs").c_str());
@@ -255,8 +269,7 @@ int main(int argc, char *argv[]){
 
             //clear colour goes first because otheriwse it covers the screen
             glClearColor( 0.16f, 0.01f, 0.11f, 0.5f);
-            glClear(GL_COLOR_BUFFER_BIT);
-
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
             if(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS){
                 opacity -= 0.05;
             }
@@ -279,7 +292,7 @@ int main(int argc, char *argv[]){
             //rotate the model 55 degrees around the x axis
             model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
             //move the world forward 3 units
-            view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+            view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -5.0f));
 
             //define our projetion matrix
             projection = glm::perspective(glm::radians(45.0f), (float)scrWidth / (float)scrHeight , 0.1f, 100.0f);
@@ -302,8 +315,19 @@ int main(int argc, char *argv[]){
 
 
             glBindVertexArray(VAO);
-            glDrawArrays(GL_TRIANGLES, 0, 36);
-            //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+            for(unsigned int i = 0; i < 10; i++)
+            {
+                model = glm::mat4(1.0f);
+                model = glm::translate(model, cubePositions[i]);
+                float angle = 20.0f * i;
+                angle += sin(glfwGetTime())*40.0f;
+
+                model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+                ourShader.setMat4("model", model);
+
+                glDrawArrays(GL_TRIANGLES, 0, 36);
+
+            }            //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 
 
